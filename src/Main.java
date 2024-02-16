e19a466136d2d32450416526bddf74d6b5778cec
RuloYat19 <3535811510101@ingenieria.usac.edu.gt>
2024-02-15 18:23:18 -0600

    Se agrega la tarea al main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int num1, num2, opc;
+        System.out.println("Ingrese un número");
+        num1 = scanner.nextInt();
+        System.out.println("Ingrese el otro número");
+        num2 = scanner.nextInt();
+
+        do {
+                System.out.println("Indique que quiere realizar");
+                System.out.println("1. Sumar");
+                System.out.println("2. Restar");
+                System.out.println("3. Multiplicar");
+                System.out.println("4. Dividir");
+                System.out.println("5. Salir");
+                opc = scanner.nextInt();
+            } while (opc < 1 || opc > 5);
+            switch (opc) {
+                case 1:
+                    System.out.println(num1+num2);
+                    break;
+                case 2:
+                    System.out.println(num1-num2);
+                    break;
+                case 3:
+                    System.out.println(num1*num2);
+                    break;
+                case 4:
+                    System.out.println(num1/num2);
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Tenga buen día");
+            }
+
+
         }
     }
-}
